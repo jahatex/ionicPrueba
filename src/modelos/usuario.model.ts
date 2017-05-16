f7ace2cb066289754a0965178e30f3542a73ff58
jahatex <jrodriguez@jrsoluciones.net>
2017-05-16 19:09:14 +0200

    Interfaz crud creada y aplicada a las clases

--- src/modelos/usuario.model.ts
+++ src/modelos/usuario.model.ts
@@ -1,7 +1,22 @@
-export class Usuario {
+import { CRUD } from "../interfaces/crud.interface";
+
+export class Usuario implements CRUD {
     id: number;
     name: string;
     surname: string;
-    imagen: boolean;
-    location: string;
+    imagen?: any;
+    location?: string;
+
+    constructor(id,name,surname,imagen?,location?){
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.imagen = (imagen!=undefined) ? imagen : null;
+        this.location = (location!=undefined) ? location : null;
+    }
+
+    create(){}
+    read(){}
+    update(){}
+    delete(){} 
 }
